8173c432cc7d0f72db6160007bab64de938740f5
Daniel.Barton446 <daniel.barton446@gmail.com>
2023-10-08 15:45:42 -0500

    feat: changes image to archlinux

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 docker build --tag nvim-container .
 docker run --rm -w /usr/local/bin -it nvim-container bash -c "ansible-playbook --ask-vault-pass personal.yml && /bin/bash"
-# docker run --rm -w /usr/local/bin -it nvim-container bash 
